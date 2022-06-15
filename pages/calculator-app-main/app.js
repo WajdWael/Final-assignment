757bc7c9d890cf943d6695ab43bd264318ccf77b
Wajd Wael <wajdabdalhay@gmail.com>
2022-06-15 21:29:27 +0300

    push updates

--- pages/calculator-app-main/app.js
+++ pages/calculator-app-main/app.js
@@ -29,8 +29,9 @@ keys.addEventListener('click', event => {
     const { type } = key.dataset
     const { previousKeyType } = calculator.dataset
     if (type === 'number') {
-        if (displayValue === '0' || previousKeyType === 'operator' || displayValue === '.') {
+        if (displayValue === '0' || displayValue == '..' || previousKeyType === 'operator') {
             display.textContent = keyValue
+            return
         } else {
             display.textContent = displayValue + keyValue
         }
@@ -92,68 +93,20 @@ function calculate(firstNumber, operator, secondNumber) {
         default:
             return
     }
-    return computation.toFixed(7)
+    return computation.toFixed(4)
 }
 
 document.onkeydown = function (event) {
-    let key_press = String.fromCharCode(event.keyCode);
+    // let key_press = String.fromCharCode(event.keyCode);
     let key_code = event.keyCode;
     let input = document.querySelector('.sc');
-    let inputVal = input.innerHTML;
-    let btnVal = this.innerHTML;
-    let lastChar = inputVal[inputVal.length - 1];
-    let operators = ['+', '-', '×', '÷'];
-    let equation = inputVal;
-    let check = inputVal != '' && operators.indexOf(lastChar) == -1;
+    // let inputVal = input.innerHTML;
+    // let btnVal = this.innerHTML;
+    // let lastChar = inputVal[inputVal.length - 1];
+    // let operators = ['+', '-', '×', '÷'];
+    // let equation = inputVal;
+    // let check = inputVal != '' && operators.indexOf(lastChar) == -1;
 
-    equation = equation.replace(/×/g, '*').replace(/÷/g, '/');
-    if(key_press==1) {
-        input.innerHTML += key_press;
-    }
-    if(key_press==2) {
-        input.innerHTML += key_press; 
-    }
-    if(key_press==3 || key_code == 32) {
-        input.innerHTML += key_press; 
-    }
-    if(key_press==4) {
-        input.innerHTML += key_press; 
-    }
-    if(key_press==5) {
-        input.innerHTML += key_press; 
-    } 
-    if(key_press==6 && event.shiftKey == false) {
-        input.innerHTML += key_press; 
-    }
-    if(key_press==7) {
-        input.innerHTML += key_press; 
-    }
-    if(key_press==8 && event.shiftKey == false) {
-        input.innerHTML += key_press; 
-    }
-    if(key_press==9) {
-        input.innerHTML += key_press; 
-    }
-    if(key_press==0) {
-        input.innerHTML += key_press;
-    }
-
-    if ((check && key_code == 187 && event.shiftKey) || (key_code == 107) || (key_code == 61 && event.shiftKey)) {
-        document.querySelector('.sc').innerHTML += '+';
-    }
-    if ((check && key_code == 189 && event.shiftKey) || (check && key_code == 107)) {
-        document.querySelector('.sc').innerHTML += '-';
-    }
-    if ((check && key_code == 56 && event.shiftKey) || (check && key_code == 106)) {
-        document.querySelector('.sc').innerHTML += '×';
-    }
-    if ((check && key_code == 191) || (check && key_code == 111)) {
-        document.querySelector('.sc').innerHTML += '÷';
-    }
-    if (key_code == 13 || key_code == 187 && event.shiftKey == false) {
-        input.innerText = eval(equation);
-        decimalAdded =false;
-    }
     if(key_code==8 || key_code==46) {
         input.innerHTML = input.innerHTML.toString().slice(0, -1)
         decimalAdded = false;
